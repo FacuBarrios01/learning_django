cfa2f5cc3a017e9f6b956809c9b22d92751d8eeb
FacuBarrios01 <facubarrios01@gmail.com>
2023-02-13 12:24:47 +0100

    Added cars post tests

--- baseApp/tests.py
+++ baseApp/tests.py
@@ -1,3 +1,103 @@
-from django.test import TestCase
+from .models import Car
+from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class CarTestCase(APITestCase):
+
+    """
+    Test suite for Car
+    """
+
+    def setUp(self):
+        self.client = APIClient()
+        self.data = {
+            "model": "Test Kuga",
+            "releaseYear": "2023-02-13",
+            "color": "sky blue",
+            "engine_model": "Testing Engine"
+        }
+        self.url = "/cars/"
+
+    def test_create_car(self):
+        '''
+        test CarViewSet create method
+        '''
+        data = self.data
+        response = self.client.post(self.url, data, format="json")
+        print(data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Car.objects.get(model="Test Kuga").model, "Test Kuga")
+
+    def test_create_car_without_model(self):
+        '''
+        test CarViewSet create method when name is not in data
+        '''
+        data = self.data
+        data.pop("model")
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_car_when_model_equals_blank(self):
+        '''
+        test CarViewSet create method when model is blank
+        '''
+        data = self.data
+        data["name"] = ""
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_car_without_message(self):
+        '''
+        test CarViewSet create method when releaseYear is not in data
+        '''
+        data = self.data
+        data.pop("releaseYear")
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_Car_when_releaseYear_equals_blank(self):
+        '''
+        test CarViewSet create method when releaseYear is blank
+        '''
+        data = self.data
+        data["releaseYear"] = ""
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_Car_without_color(self):
+        '''
+        test CarViewSet create method when color is not in data
+        '''
+        data = self.data
+        data.pop("color")
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_Car_when_color_equals_blank(self):
+        '''
+        test CarViewSet create method when color is blank
+        '''
+        data = self.data
+        data["color"] = ""
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_Car_without_engine_model(self):
+        '''
+        test CarViewSet create method when engine_model is not in data
+        '''
+        data = self.data
+        data.pop("engine_model")
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_Car_when_engine_model_equals_blank(self):
+        '''
+        test CarViewSet create method when engine_model is blank
+        '''
+        data = self.data
+        data["engine_model"] = ""
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- mysite/settings.py
+++ mysite/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_extensions'
+    # 'django_extensions'
     'django_filters',
     'baseApp.apps.BaseappConfig',
     'rest_framework'
@@ -132,24 +132,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
-    'DEFAULT_PARSER_CLASSES': (
-        'rest_framework_json_api.parsers.JSONParser',
-    ),
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework_json_api.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer'
-    ),
-    'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': (
-        'rest_framework_json_api.filters.QueryParameterValidationFilter',
-        'rest_framework_json_api.filters.OrderingFilter',
-        'rest_framework_json_api.django_filters.DjangoFilterBackend',
-        'rest_framework.filters.SearchFilter',
-    ),
-    'SEARCH_PARAM': 'filter[search]',
-    'TEST_REQUEST_RENDERER_CLASSES': (
-        'rest_framework_json_api.renderers.JSONRenderer',
-    ),
-    'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json'
+        'django_filters.rest_framework.DjangoFilterBackend',
+    )
 }
